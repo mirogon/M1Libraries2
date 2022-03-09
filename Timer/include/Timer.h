32c8431f6544c8add23a0d39bd556ecd5ef14b0f
mirogon <m1smr@hotmail.com>
2022-03-09 02:40:24 +0100

    Timer finished

--- Timer/include/Timer.h
+++ Timer/include/Timer.h
@@ -1,7 +1,40 @@
 #pragma once
+#include <chrono>
+#include <stdint.h>
 
 namespace m1{
     class Timer{
+
+        public:
+
+        Timer():
+            startTime{std::chrono::high_resolution_clock::now()}
+        {
+        }
+
+        void Restart(){
+            startTime = std::chrono::high_resolution_clock::now();
+        }
+
+        uint64_t ElapsedNano(){
+            return std::chrono::duration_cast<std::chrono::duration<uint64_t, std::ratio<1,1000000000>>>(std::chrono::high_resolution_clock::now() - startTime).count();
+        }
+
+        uint64_t ElapsedMicro(){
+            return std::chrono::duration_cast<std::chrono::duration<uint64_t, std::ratio<1,1000000>>>(std::chrono::high_resolution_clock::now() - startTime).count();
+        };
+
+        uint64_t ElapsedMilli(){
+            return std::chrono::duration_cast<std::chrono::duration<uint64_t, std::ratio<1,1000>>>(std::chrono::high_resolution_clock::now() - startTime).count();
+        }
+
+        uint64_t ElapsedSec(){
+            return std::chrono::duration_cast<std::chrono::duration<uint64_t, std::ratio<1,1>>>(std::chrono::high_resolution_clock::now() - startTime).count();
+        }
+
+        private:
+
+        std::chrono::high_resolution_clock::time_point startTime;
 
     };
 }
--- Timer/test/test.cpp
+++ Timer/test/test.cpp
@@ -1,7 +1,39 @@
 #include <catch2/catch_test_macros.hpp>
+#include <thread>
 #include "../include/Timer.h"
 
 TEST_CASE("NewTimerValid"){
     m1::Timer timer = m1::Timer();
     REQUIRE_FALSE(&timer == nullptr);
+}
+
+TEST_CASE("NewTimerTimeAboveZeroMicro"){
+    m1::Timer timer = m1::Timer();
+    REQUIRE(timer.ElapsedMicro() >= 0);
+}
+
+TEST_CASE("TimerAbove10MicroAfterWaiting11Micro"){
+    m1::Timer timer = m1::Timer();
+    std::this_thread::sleep_for(std::chrono::microseconds(11));
+    REQUIRE(timer.ElapsedMicro() > 10);
+}
+
+TEST_CASE("TimerElapsedMilliEqualsWaitedTime"){
+    m1::Timer timer = m1::Timer();
+    std::this_thread::sleep_for(std::chrono::milliseconds(3));
+    REQUIRE(timer.ElapsedMilli() > 2);
+    REQUIRE(timer.ElapsedMilli() < 100);
+}
+
+TEST_CASE("TimerElapsedSecIsZeroAfterInstantiation"){
+    m1::Timer timer = m1::Timer();
+    REQUIRE(timer.ElapsedSec() == 0);
+}
+
+TEST_CASE("TimerElapsedMilliAboutZeroAfterRestartingTimer"){
+    m1::Timer timer = m1::Timer();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    REQUIRE(timer.ElapsedMilli() >= 50);
+    timer.Restart();
+    REQUIRE(timer.ElapsedMilli() <= 25);
 }
